132fe5995dac43844c2d4ae3b63a04f3c8cc2910
Briana Dodgen <brianadodgen@gmail.com>
2022-09-21 17:36:25 +0200

    migrated work over to avoid gulp problems

---
+++ js/buying-a-product-script.js
@@ -0,0 +1,13 @@
+// Create a page with an image of some kind of a product like you would see in an e - shop. (You can steal an image from the internet.I won't tell.) Put a button with label Buy underneath the product. When the user clicks on the button display an alert: "Product added to cart".
+
+// Instead of displaying and alert change the label on the button to "In cart".
+
+// Create two different CSS classes for your button so that the button can have a different color when the product is in cart.When the users click on the button, change not only its label but also its class.
+
+// Add a text field beside the buy button to allow the user to input quantity(for example in pieces).When the user clicks the button, its label will change to something like "3 pieces in cart".
+
+console.log("js is connected")
+
+const buttonClick = () => {
+  alert("Product added to cart")
+}
